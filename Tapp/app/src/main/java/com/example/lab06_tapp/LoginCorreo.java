c0d92da106044ffb0807c7537f22ccc41e58732b
arielrojasg <arielrojasg18@gmail.com>
2019-05-10 01:38:48 -0600

    se agrego firebase para login y autenticacion

--- Tapp/app/src/main/java/com/example/lab06_tapp/LoginCorreo.java
+++ Tapp/app/src/main/java/com/example/lab06_tapp/LoginCorreo.java
@@ -1,20 +1,88 @@
 package com.example.lab06_tapp;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginCorreo extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    EditText usernameEditText,passwordEditText;
+    ProgressBar progressBar;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_correo);
 
-        final EditText usernameEditText = findViewById(R.id.username);
-        final EditText passwordEditText = findViewById(R.id.password);
-        final Button loginButton = findViewById(R.id.login);
+        mAuth = FirebaseAuth.getInstance();
+
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+
+        loginButton = findViewById(R.id.loginBtn);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Login();
+            }
+        });
+    }
+
+    public void Login(){
+        String user = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        if(user.isEmpty()){
+            usernameEditText.setError("Digite un correo");
+            usernameEditText.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(user).matches()){
+            usernameEditText.setError("Digite un correo válido");
+            usernameEditText.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            passwordEditText.setError("Digite una contraseña");
+            passwordEditText.requestFocus();
+            return;
+        }
+
+        if(password.length() < 5){
+            passwordEditText.setError("La contraseña debe tener al menos 5 caracteres");
+            passwordEditText.requestFocus();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(user,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.VISIBLE);
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(LoginCorreo.this, Home.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    LoginCorreo.this.startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(), "Credenciales incorrectos",Toast.LENGTH_LONG).show();
+                }
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+
     }
 }
--- Tapp/app/src/main/java/com/example/lab06_tapp/Registro_Usuario.java
+++ Tapp/app/src/main/java/com/example/lab06_tapp/Registro_Usuario.java
@@ -1,22 +1,34 @@
 package com.example.lab06_tapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 public class Registro_Usuario extends AppCompatActivity {
 
+    EditText correo, password;
     Button registrarseBtn;
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
 
         registrarseBtn = (Button)findViewById(R.id.registrobtn);
+        correo = (EditText) findViewById(R.id.correotxt);
+        password = (EditText) findViewById(R.id.passwordtxt);
         registrarseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,11 +36,51 @@ public class Registro_Usuario extends AppCompatActivity {
             }
         });
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void goToMain(){
-        Intent intent = new Intent(Registro_Usuario.this, MainActivity.class);
-        Registro_Usuario.this.startActivity(intent);
-        Toast.makeText(Registro_Usuario.this, "Se ha registrado correctamente", Toast.LENGTH_LONG).show();
+        String mail = correo.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+
+        if(mail.isEmpty()){
+            correo.setError("Digite un correo");
+            correo.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(mail).matches()){
+            correo.setError("Digite un correo válido");
+            correo.requestFocus();
+            return;
+        }
+        if(pass.isEmpty()){
+            password.setError("Digite una contraseña");
+            password.requestFocus();
+            return;
+        }
+
+        if(pass.length() < 5){
+            password.setError("La contraseña debe tener al menos 5 caracteres");
+            password.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(mail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "Se ha registrado correctamente",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(Registro_Usuario.this, MainActivity.class);
+                    Registro_Usuario.this.startActivity(intent);
+                }else{
+                    if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                        Toast.makeText(getApplicationContext(), "Ya hay un usuario registrado con ese correo",Toast.LENGTH_LONG).show();
+                    }else {
+                        Toast.makeText(getApplicationContext(), "Datos incompletos", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
     }
 }
